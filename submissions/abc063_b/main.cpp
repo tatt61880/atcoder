151bbd1c2c2357abb2201380ceec6b47c73e3d0a
tatt61880 <tatt61880@gmail.com>
2018-01-29 01:19:58 +0900

    .

--- submissions/abc063_b/main.cpp
+++ submissions/abc063_b/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <string>
 #include <set>
 int main()
 {
- std::string str;
- std::cin >> str;
- std::set<char> s;
- for(int i = 0; i < str.size(); i++){
-  s.insert(str[i]);
- }
- std::cout << ((s.size() == str.size()) ? "yes" : "no") << std::endl;
- return 0;
+	std::string str;
+	std::cin >> str;
+	std::set<char> s;
+	for(int i = 0; i < str.size(); i++){
+		s.insert(str[i]);
+	}
+	std::cout << ((s.size() == str.size()) ? "yes" : "no") << std::endl;
+	return 0;
 }
