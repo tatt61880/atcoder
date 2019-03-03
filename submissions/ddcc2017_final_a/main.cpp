96aefeb6eca6f958e2dc4c55323f5807d7a2b068
tatt61880 <tatt61880@gmail.com>
2019-03-03 19:32:20 +0900

    .

--- submissions/ddcc2017_final_a/main.cpp
+++ submissions/ddcc2017_final_a/main.cpp
@@ -1,47 +1,48 @@
-#include <iostream>
-#include <cstring>
-#include <algorithm>
-#include <vector>
+//{{{
+#include <bits/stdc++.h>
 using namespace std;
+#define repX(a,b,c,x,...) x
+#define repN(a) repX a
+#define rep(...) repN((__VA_ARGS__,rep3,rep2,loop))(__VA_ARGS__)
+#define rrep(...) repN((__VA_ARGS__,rrep3,rrep2))(__VA_ARGS__)
+#define loop(n) rep2(i_,n)
+#define rep2(i,n) rep3(i,0,n)
+#define rep3(i,begin,end) for(int i=(int)(begin),i##_end=(int)(end);i<i##_end;++i)
+#define rrep2(i,n) rrep3(i,n,0)
+#define rrep3(i,begin,end) for(int i=(int)(begin-1),i##_end=(int)(end);i>=i##_end;--i)
+//}}}
 
-#define PrintLn(X) cout << X << endl
-#define Rep(i, n) for(int i = 0; i < (int)(n); ++i)
-#define For(i, a, b) for(int i = a;
- i < (int)(b); ++i)
+int K;
 
-typedef long long ll;
-
-float K;
-
-bool in(float x, float y, float r)
+bool in(int x, int y, int r)
 {
-	return x * x + y * y <= r * r;
+  return x * x + y * y <= r * r;
 }
 
-int num(float size)
+int num(int size)
 {
-	int res = 0;
-	float r = size / 2.0;
-	Rep(i, size / K){
-		Rep(j, size / K){
-			float min_x = i * K - r;
-			float min_y = j * K - r;
-			float max_x = (i + 1) * K - r;
-			float max_y = (j + 1) * K - r;
-			if(in(min_x, min_y, r) && 
-				in(max_x, min_y, r) && 
-				in(min_x, max_y, r) && 
-				in(max_x, max_y, r)){
-				res++;
-			}
-		}
-	}
-	return res;
+  int res = 0;
+  int r = size / 2;
+  rep(i, size / K){
+    rep(j, size / K){
+      int min_x = i * K - r;
+      int min_y = j * K - r;
+      int max_x = min_x + K;
+      int max_y = min_y + K;
+      if(in(min_x, min_y, r) &&
+          in(max_x, min_y, r) &&
+          in(min_x, max_y, r) &&
+          in(max_x, max_y, r)){
+        res++;
+      }
+    }
+  }
+  return res;
 }
 
 int main(void)
 {
-	cin >> K;
-	cout << num(200.0) << " " << num(300.0) << endl;
-	return 0;
+  cin >> K;
+  cout << num(200) << " " << num(300) << endl;
+  return 0;
 }
