6e29672085d9d5a6f07f683d6890915647f4c25a
tatt61880 <tatt61880@gmail.com>
2019-03-03 20:21:07 +0900

    .

---
+++ submissions/_cmd.bat
@@ -0,0 +1,2 @@
+@echo off
+cmd.exe /s /k "prompt $P$_$T$G$S&& chcp 65001 && "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat""
--- submissions/abc111_a/main.cpp
+++ submissions/abc111_a/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
   string s;
   cin >> s;
-  for(auto &x: s){
+  for (auto &x: s) {
     cout << (x == '1' ? 9 : 1);
   }
   cout << endl;
