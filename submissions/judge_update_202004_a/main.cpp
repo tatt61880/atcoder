2efb6678fddbce3f378863e49e8fc8bf4f14efd4
tatt61880 <tatt61880@gmail.com>
2020-04-11 23:39:49 +0900

    .

---
+++ submissions/judge_update_202004_a/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int s, l, r;
+  cin >> s >> l >> r;
+  if(s < l) s = l;
+  if(s > r) s = r;
+  cout << s << endl;
+}
---
+++ submissions/judge_update_202004_b/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main(){
+  int n;
+  cin >> n;
+  vector<int> r;
+  vector<int> b;
+  for(int i = 0; i < n; ++i){
+    int x;
+    char c;
+    cin >> x >> c;
+    (c == 'R' ? r : b).push_back(x);
+  }
+  sort(r.begin(), r.end());
+  sort(b.begin(), b.end());
+  for(int i = 0; i < r.size(); ++i) cout << r[i] << endl;
+  for(int i = 0; i < b.size(); ++i) cout << b[i] << endl;
+}
