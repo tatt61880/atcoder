0b458530ce884c0a8fb3b814966722f4950512d3
tatt61880 <tatt61880@gmail.com>
2026-05-16 12:49:04 +0900

    .

--- update_data.bat
+++ update_data.bat
@@ -7,9 +7,11 @@ if "%DAYS%"=="" (
   python save_submissions.py %DAYS%
 )
 
-python save_contests.py
-python save_problems.py
-python save_contest_problem.py
+if not "%1" == "skip" (
+  python save_contests.py
+  python save_problems.py
+  python save_contest_problem.py
+)
 
 atcoder.exe
 
